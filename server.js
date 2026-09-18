b97300943e1acf280f20e94d0693eb0c852480f4
raydennthomas-cmyk <raydennthomas@gmail.com>
2026-09-18 11:32:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const TARGET_URL = 'https://crazygames.com'; 
+const TARGET_URL = 'https://youtube.com'; 
 
 const gameProxy = createProxyMiddleware({
     target: TARGET_URL,
